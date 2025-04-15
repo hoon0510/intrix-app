c78d1d744157310778227840c3f72cbba58b3325
seonghun-jeong <hoon0510@gmail.com>
2025-04-15 23:21:53 +0900

    fix: tsconfig.json과 next.config.js의 경로 설정 수정

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,1 +1,17 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig; 
